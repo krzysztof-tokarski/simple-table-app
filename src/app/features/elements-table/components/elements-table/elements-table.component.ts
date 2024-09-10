7ab7ae5d3e19053adca6d096dd3fe8bc120d4152
krzysztof.tokarski <krzysztof.tokarski96@gmail.com>
2024-09-10 19:53:51 +0200

    feat: add delay to filtering and some minor improvements/reorg

--- src/app/features/elements-table/components/elements-table/elements-table.component.ts
+++ src/app/features/elements-table/components/elements-table/elements-table.component.ts
@@ -1,3 +1,4 @@
+import { JsonPipe } from '@angular/common';
 import {
   Component,
   computed,
@@ -6,15 +7,19 @@ import {
   signal,
   WritableSignal,
 } from '@angular/core';
-import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
-import { FormsModule } from '@angular/forms';
+import {
+  takeUntilDestroyed,
+  toObservable,
+  toSignal,
+} from '@angular/core/rxjs-interop';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
-import { delay, first, tap } from 'rxjs';
+import { debounceTime, delay, first, tap } from 'rxjs';
 import { TABLE_COLUMNS, TABLE_CONFIG } from '../../constants/table-config';
 import { PERIODIC_ELEMENT_FILTERABLE_PROPERTIES } from '../../models/periodic-element';
 import { ElementsService } from '../../services/elements-service.service';
@@ -33,7 +38,7 @@ const MATERIAL_IMPORTS = [
 @Component({
   selector: 'app-elements-table',
   standalone: true,
-  imports: [MATERIAL_IMPORTS, FormsModule],
+  imports: [MATERIAL_IMPORTS, FormsModule, ReactiveFormsModule, JsonPipe],
   templateUrl: './elements-table.component.html',
   styleUrl: './elements-table.component.scss',
 })
@@ -41,7 +46,10 @@ export class ElementsTableComponent implements OnInit {
   protected TABLE_CONFIG = TABLE_CONFIG;
   protected TABLE_COLUMNS = TABLE_COLUMNS;
   protected elements: WritableSignal<PeriodicElement[]> = signal([]);
-  protected value: WritableSignal<string> = signal('');
+  protected filterCtrl = new FormControl('');
+  protected filteredValue = toSignal(
+    this.filterCtrl.valueChanges.pipe(debounceTime(2_000))
+  );
 
   protected filteredElements = computed(() => {
     return this.elements().filter((el) => {
@@ -54,10 +62,14 @@ export class ElementsTableComponent implements OnInit {
         )
         .reduce((acc, [, value]) => acc + value, '')
         .toLowerCase()
-        .includes(this.value().toLowerCase());
+        .includes((this.filteredValue() ?? '').toLowerCase());
     });
   });
-  protected elements$ = toObservable(this.filteredElements).pipe();
+  protected elements$ = toObservable(this.filteredElements);
+
+  protected onClearClick() {
+    this.filterCtrl.setValue('');
+  }
 
   public ngOnInit(): void {
     getElements.call(this);
@@ -68,11 +80,9 @@ export class ElementsTableComponent implements OnInit {
   }
   private _elements$ = this._elementsService.getElements$().pipe(
     takeUntilDestroyed(this._destroyRef),
-    delay(0),
+    delay(100),
     first(),
-    tap((elements) =>
-      this.elements.set(structuredClone(elements) as PeriodicElement[])
-    )
+    tap((elements) => this.elements.set(elements))
   );
 
   constructor(
--- src/app/features/elements-table/services/elements-service.service.ts
+++ src/app/features/elements-table/services/elements-service.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 import { ELEMENT_DATA } from '../constants/elements';
+import { PeriodicElement } from '../models/periodic-element';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ElementsService {
   public getElements$() {
-    return of(ELEMENT_DATA);
+    return of(structuredClone(ELEMENT_DATA) as PeriodicElement[]);
   }
 }
